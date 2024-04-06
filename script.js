c3188c87148c413b8510760deeb193419bc53239
IceCreamBaby <129086773+IceCreamBaby@users.noreply.github.com>
2024-04-06 15:50:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function checkDate(event) {
     event.preventDefault();
-    var today = new Date();
     var selectedDate = new Date(document.getElementById('date').value);
-    var florianBirthday = new Date(today.getFullYear(), 7, 17); // 17.08 ohne Jahr
 
-    if (selectedDate.getDate() == today.getDate() && selectedDate.getMonth() == today.getMonth()) {
+    if (selectedDate.getDate() == 7 && selectedDate.getMonth() == 3) {
         window.location.href = 'frage.html';
     } else {
-        alert('Bitte wähle das heutige Datum aus.');
+        alert('Bitte wähle den 07.04 aus.');
     }
 }
 
@@ -17,7 +15,6 @@ window.onload = function() {
         birthdayForm.onsubmit = function(event) {
             event.preventDefault();
             var selectedDate = new Date(document.getElementById('birthdate').value);
-            var florianBirthday = new Date(selectedDate.getFullYear(), 7, 17); // 17.08 ohne Jahr
 
             if (selectedDate.getDate() == 17 && selectedDate.getMonth() == 7) {
                 window.location.href = 'geburtstag.html';
