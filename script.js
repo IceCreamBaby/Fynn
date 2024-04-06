e52108d1eaea6080231a1df2e1462d533fd15260
IceCreamBaby <129086773+IceCreamBaby@users.noreply.github.com>
2024-04-06 15:18:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-window.onload = function() {
+function checkDate(event) {
+    event.preventDefault();
     var today = new Date();
     var selectedDate = new Date(document.getElementById('date').value);
     var florianBirthday = new Date(today.getFullYear(), 7, 17); // 17.08 ohne Jahr
 
     if (selectedDate.getDate() == today.getDate() && selectedDate.getMonth() == today.getMonth()) {
         window.location.href = 'frage.html';
+    } else {
+        alert('Bitte wähle das heutige Datum aus.');
+    }
+}
+
+window.onload = function() {
+    var birthdayForm = document.getElementById('birthdayForm');
+    if (birthdayForm) {
+        birthdayForm.onsubmit = function(event) {
+            event.preventDefault();
+            var selectedDate = new Date(document.getElementById('birthdate').value);
+            var florianBirthday = new Date(selectedDate.getFullYear(), 7, 17); // 17.08 ohne Jahr
+
+            if (selectedDate.getDate() == 17 && selectedDate.getMonth() == 7) {
+                window.location.href = 'geburtstag.html';
+            } else {
+                alert('Bitte wähle den richtigen Geburtstag aus.');
+            }
+        };
     }
 };
-
