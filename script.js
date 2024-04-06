e1f7f191167dda1d232c11a6aa1edd9ab12e570f
IceCreamBaby <129086773+IceCreamBaby@users.noreply.github.com>
2024-04-06 14:58:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 window.onload = function() {
     var today = new Date();
-    var birthdayDate = new Date('2024-04-06');
-    var florianBirthday = new Date('2024-08-17');
+    var selectedDate = new Date(document.getElementById('date').value);
+    var florianBirthday = new Date(today.getFullYear(), 7, 17); // 17.08 ohne Jahr
 
-    if (today.getDate() == birthdayDate.getDate() && today.getMonth() == birthdayDate.getMonth()) {
+    if (selectedDate.getDate() == today.getDate() && selectedDate.getMonth() == today.getMonth()) {
         window.location.href = 'frage.html';
-    } else {
-        document.querySelector('.container').innerHTML = '<h2>Du hast noch nicht Geburtstag!!!</h2>';
-    }
-
-    if (window.location.href.includes('frage.html') && today.getDate() == florianBirthday.getDate() && today.getMonth() == florianBirthday.getMonth()) {
-        window.location.href = 'geburtstag.html';
     }
 };
+
